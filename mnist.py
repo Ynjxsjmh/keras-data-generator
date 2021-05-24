76eb865ed513ccde6aba62159bbd7f9112327620
Ynjxsjmh <ynjxsjmh@gmail.com>
2021-05-24 16:23:45 +0800

    Variable name to lower case

--- mnist.py
+++ mnist.py
@@ -17,32 +17,32 @@ RESHAPED = 784
 def process_dataset():
     # Loading MNIST dataset
     mnist = keras.datasets.mnist
-    (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
     # You can verify that the split between train and test is 60,000, and 10,000 respectively.
-    assert X_train.shape == (60000, 28, 28)
-    assert X_test.shape == (10000, 28, 28)
-    assert Y_train.shape == (60000,)
-    assert Y_test.shape == (10000,)
+    assert x_train.shape == (60000, 28, 28)
+    assert x_test.shape == (10000, 28, 28)
+    assert y_train.shape == (60000,)
+    assert y_test.shape == (10000,)
 
-    X_train = X_train.reshape(X_train.shape[0], RESHAPED)
-    X_test = X_test.reshape(X_test.shape[0], RESHAPED)
-    X_train = X_train.astype('float32')
-    X_test = X_test.astype('float32')
+    x_train = x_train.reshape(x_train.shape[0], RESHAPED)
+    x_test = x_test.reshape(x_test.shape[0], RESHAPED)
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
 
     # Normalize inputs to be within in [0, 1].
-    X_train /= 255
-    X_test /= 255
+    x_train /= 255
+    x_test /= 255
 
-    return (X_train, Y_train), (X_test, Y_test)
+    return (x_train, y_train), (x_test, y_test)
 
 
 def fit_normal():
-    (X_train, Y_train), (X_test, Y_test) = process_dataset()
+    (x_train, y_train), (x_test, y_test) = process_dataset()
 
     # One-hot representation of the labels.
-    Y_train = tf.keras.utils.to_categorical(Y_train, CLASSES_NUM)
-    Y_test = tf.keras.utils.to_categorical(Y_test, CLASSES_NUM)
+    y_train = tf.keras.utils.to_categorical(y_train, CLASSES_NUM)
+    y_test = tf.keras.utils.to_categorical(y_test, CLASSES_NUM)
 
     # Build the model
     model = tf.keras.models.Sequential()
@@ -59,17 +59,17 @@ def fit_normal():
                   metrics=['accuracy'])
 
     # Train the model
-    model.fit(X_train, Y_train,
+    model.fit(x_train, y_train,
               batch_size=BATCH_SIZE, epochs=EPOCHS,
               verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
 
     # Evaluate the model
-    test_loss, test_acc = model.evaluate(X_test, Y_test)
+    test_loss, test_acc = model.evaluate(x_test, y_test)
     print('Test loss:', test_loss)
     print('Test accuracy:', test_acc)
 
     # Make prediction
-    predictions = model.predict(X_test)
+    predictions = model.predict(x_test)
 
 
 if __name__ == "__main__":
