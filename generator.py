3a0cc13222e6884aa68d404a6ccfec2f674b904a
Ynjxsjmh <ynjxsjmh@gmail.com>
2021-05-24 21:02:56 +0800

    Subclass keras.utils.Sequence
    Document: [tf.keras.utils.Sequence | TensorFlow Core v2.4.1](https://web.archive.org/web/20210420085858/https://www.tensorflow.org/api_docs/python/tf/keras/utils/Sequence)
    
    Example:
    
    ```python
    from skimage.io import imread
    from skimage.transform import resize
    import numpy as np
    import math
    
    class CIFAR10Sequence(Sequence):
    
        def __init__(self, x_set, y_set, batch_size):
            self.x, self.y = x_set, y_set
            self.batch_size = batch_size
    
        def __len__(self):
            return math.ceil(len(self.x) / self.batch_size)
    
        def __getitem__(self, idx):
            batch_x = self.x[idx * self.batch_size:(idx + 1) *
            self.batch_size]
            batch_y = self.y[idx * self.batch_size:(idx + 1) *
            self.batch_size]
    
            return np.array([
                resize(imread(file_name), (200, 200))
                   for file_name in batch_x]), np.array(batch_y)
    ```

---
+++ generator.py
@@ -0,0 +1,38 @@
+import numpy as np
+import tensorflow as tf
+
+
+class DataGenerator(tf.keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, x, y, classes_num,
+                  batch_size=32, shuffle=True):
+        'Initialization'
+        self.x = x
+        self.y = y
+
+        self.batch_size = batch_size
+        self.classes_num = classes_num
+        self.shuffle = shuffle
+
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.x) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        batch_x = [self.x[idx] for idx in indexes]
+        batch_y = [self.y[idx] for idx in indexes]
+
+        return np.array(batch_x), tf.keras.utils.to_categorical(batch_y, num_classes=self.classes_num)
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.x))
+
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
